9233d2fdd19c592dce4814256d2698b1f134cd11
Nathan Mo <moqizhengz@gmail.com>
2026-05-07 00:10:23 +0800

    fix: build err

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 确保 GitHub Pages 路径正确
+  base: '/', // 根域名部署
 })
